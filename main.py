4a7a02f135ad56a7c7255d5a4cbd1f249ef3100c
Amagash <tiffanysouterre@gmail.com>
2021-07-15 01:01:36 +0200

    get closest matches

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 import json
-from difflib import SequenceMatcher
+from difflib import SequenceMatcher, get_close_matches
 
 def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
     else:
-        return "The word doesn't exist."
+        matches = get_close_matches(word, data)
+        return "The word {} doesn't exist. Did you mean {}?".format(word, matches[0])
 
-def best_similarity(data, word):
-    word = word.lower()
-    best_ratio = 0
-    best_match = ""
-    for item in data:
-        match = SequenceMatcher(None, word, item).ratio()
-        if match > best_ratio:
-            best_ratio = match
-            best_match = item
-    print (best_match, best_ratio)
+# def best_similarity(data, word):
+#     word = word.lower()
+#     best_ratio = 0
+#     best_match = ""
+#     for item in data:
+#         match = SequenceMatcher(None, word, item).ratio()
+#         if match > best_ratio:
+#             best_ratio = match
+#             best_match = item
+#     print (best_match, best_ratio)
 
 
 data = json.load(open("data.json"))
 word = 'rainn'
 # word = input("input word: ")
 
-# print(translate(word))
-
-best_similarity(data, word)
+print(translate(word))
